c24a3ce27b13c2d6e687f6ebf74583803248ec81
David Filla <d.filla@phytec.de>
2018-05-15 10:52:23 +0200

    commit

--- app/src/main/java/de/tecmine/spielplatzparty/SpielplatzParty.java
+++ app/src/main/java/de/tecmine/spielplatzparty/SpielplatzParty.java
@@ -1,11 +1,12 @@
 package de.tecmine.spielplatzparty;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SpielplatzParty {
+public class SpielplatzParty extends Activity {
 
     protected void onCreate(Bundle bundle){
-
+        super.onCreate(bundle);
     }
 
 }
